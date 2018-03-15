30607a75c3b755f4e42a8316883c90c448fa55a3
Sanguchi <22matias1994@gmail.com>
2018-03-15 19:28:32 -0300

    Need a observer pattern

--- src/telehx/TeleHxBot.hx
+++ src/telehx/TeleHxBot.hx
@@ -1,13 +1,42 @@
 package telehx;
+using TeleHxMethods;
 
-interface TeleHxPlugin {
-    public function messageHandler(sender: TeleHxBot, message: String): Bool;
+// TODO: Haxe switch to redirect the type to the correct handler
+interface TeleHxTypeHandler<T> {
+    public function typeHandler(sender: TeleHxBot, object: T);
+}
+interface TeleHxUpdateHandler implements TeleHxTypeHandler {
+    public function updateHandler(sender: TeleHxBot, update: HxUpdate): Bool;
+}
+
+interface TeleHxMessageHandler implements TeleHxTypeHandler {
+    public function messageHandler(sender: TeleHxBot, message: HxMessage): Bool;
+}
+
+interface TeleHxInlineQueryHandler implements TeleHxTypeHandler {
+    public function inlineQueryHandler(sender: TeleHxBot, inlineQuery: HxInlineQuery): Bool;
+}
+
+interface TeleHxChosenInlineResultHandler implements TeleHxTypeHandler {
+    public function chosenInlineResultHandler(sender: TeleHxBot, chosenInlineQueryResult: HxChosenInlineResult): Bool;
+}
+
+interface TeleHxCallbackQueryHandler implements TeleHxTypeHandler {
+    public function callbackQueryHandler(sender: TeleHxBot, callbackQuery: HxCallbackQuery): Bool;
+}
+
+interface TeleHxShippingQueryHandler implements TeleHxTypeHandler {
+    public function shippingQueryHandler(sender: TeleHxBot, shippingQuery: HxShippingQuery): Bool;
+}
+
+interface TeleHxPreCheckoutQuery implements TeleHxTypeHandler {
+    public function preCheckoutQuery(sender: TeleHxBot, checkoutQuery: HxPreCheckoutQuery): Bool;
 }
 
 class TeleHxBot {
     public var token(default, null): String;
-    public var plugins(default, null): Array<TeleHxPlugin> = [];
-
+    public var plugins(default, null): Map<TeleHxTypeHandler> = [];
+    private var running: Bool = false;
     public function new(token: String) {
         this.token = token;
     }
@@ -19,8 +48,25 @@ class TeleHxBot {
             }
         }
     }
-
+    
     public function addPlugin(plugin: TeleHxPlugin): Void {
         plugins.push(plugin);
+    }
+    
+    public function stopPolling(): Void {
+        this.running = false;
+    }
+    
+    public function startPolling(): Void {
+        this.running = true;
+        var update_offset: Int = 0;
+        while(this.running) {
+            this.getUpdates({offset: update_offset}, function(updates: Array<HxUpdate>){
+                for(update in updates) {
+                    update_offset = update.update_id;
+                    
+                }
+            });
+        }
     }
 }
