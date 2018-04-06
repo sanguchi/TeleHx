648991b85194cec83d2db75257b50063dc241756
Sanguchi <22matias1994@gmail.com>
2018-04-05 21:25:05 -0300

    Standalone webserver

--- examples/WebhookExample.hx
+++ examples/WebhookExample.hx
@@ -1,15 +1,19 @@
 import telehx.TeleHxTypes;
 import telehx.TeleHxBot;
-import neko.Web;
+import tink.http.containers.*;
+import tink.http.Request;
+import tink.http.Response;
+using tink.CoreApi;
 using telehx.TeleHxMethods;
 
 class WebhookExample {
     static function main() {
         var token :String = sys.io.File.getContent('../token.txt');
         var owner_id: String = sys.io.File.getContent('../owner.txt');
-        var bot: TeleHxBot = new TeleHxBot(token);
-        var data: String = Web.getPostData();
+        var webhook_url: String = sys.io.File.getContent('../webhook.txt');
         
+        var bot: TeleHxBot = new TeleHxBot(token);
+        bot.setWebhook({url: webhook_url});
         bot.addHandler(function(update: HxUpdate): Bool {
             switch update {
                 case {message: {text: text}} if(text != null): {
@@ -22,11 +26,31 @@ class WebhookExample {
             }
         });
         
-        if(data != null) {
-            var update: HxUpdate = haxe.Json.parse(data);
-            bot.notifyPlugins(update);
-            Web.setReturnCode(200);
-        }
-        
+        var container: TcpContainer = new TcpContainer(8080);
+        container.run(function(request: IncomingRequest){
+            // var update: HxUpdate = haxe.Json.parse(request.body.toString());
+            // bot.notifyPlugins(update);
+           //  trace(request.body);
+            switch request.body {
+                case Plain(src): {
+                    src.all().map(function(o){
+                        switch(o) {
+                            case Success(data): {
+                                // trace('data: ${data.toString()}');
+                                var update: HxUpdate = haxe.Json.parse(data.toString());
+                                bot.notifyPlugins(update);
+                            }
+                            case Failure(e): {
+                                trace('Error: $e');
+                            }
+                        }
+                    });
+                }
+                case Parsed(parts): {
+                    trace(parts);
+                }
+            }
+            return Future.sync(('':OutgoingResponse));
+        });
     }
 }
